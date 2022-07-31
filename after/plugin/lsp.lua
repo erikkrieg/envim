0540613cc1599235fd9b3c20e856fbd1480afef3
Erik Krieg <erik.c.krieg@gmail.com>
2022-07-31 17:21:07 -0400

    re-enables completion item snippet support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,6 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities( capabilities)
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local function config(c)
   return c or {}
