26f196d3880d2c39f52d2d1ffcefaa4ff9dabb54
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-22 01:17:59 -0400

    adds keymaps for telescope

--- lua/erikkrieg/init.lua
+++ lua/erikkrieg/init.lua
@@ -1,3 +1,4 @@
 require('erikkrieg.plugins')
 require('erikkrieg.options')
 require('erikkrieg.autocmd')
+require('erikkrieg.keymaps')
---
+++ lua/erikkrieg/keymaps.lua
@@ -0,0 +1,14 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal
+-- Telescope
+keymap("n", "<leader>t", "<cmd>Telescope<cr>", opts)
+keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>g", "<cmd>Telescope live_grep<cr>", opts)
+
