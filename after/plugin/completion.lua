7148237974309b090adee2dfa0be0db9c0ce5c91
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-24 00:23:26 -0400

    change cmp return keymap to only confirm if a selection was made

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -41,7 +41,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm(),
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
   }),
