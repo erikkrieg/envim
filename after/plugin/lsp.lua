5b4709b6e3e4c5e66f3821882a49b90673a4a94b
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-25 00:14:35 -0400

    tidy lsp by using config function for rust and capabilities

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,28 +28,26 @@ end
 local function config(c)
   cfg = c or {}
   cfg.on_attach = on_attach
+  cfg.capabilities = capabilities
   return cfg
 end
 
 -- Rust
 require('rust-tools').setup({
-    server = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-            ["rust-analyzer"] = {
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
+    server = config({
+      settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = {
+            command = "clippy"
+          },
         }
-    },
+      }
+    }),
 })
 
 -- Go
 require("lspconfig").gopls.setup(config({
 	cmd = { "gopls", "serve" },
-  capabilities = capabilities,
 	settings = {
 		gopls = {
 			analyses = {
