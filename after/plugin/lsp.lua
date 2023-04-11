ba9d4e121e13960cf73918a5dde78c1479f382db
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-04-11 19:04:00 -0400

    update lsp buffer format to user newer format method

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,7 +20,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>F', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, bufopts)
 end
 
 local function config(c)
