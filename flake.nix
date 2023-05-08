ea1a7e24cf3d872a5b86464c3125f234827e3d85
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-08 00:20:40 -0400

    creates envim flake using nixpkgs neovim-unwrapped

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Erik's Neovim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, ... }: 
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in with pkgs; rec {
+      packages.envim = wrapNeovim neovim-unwrapped {
+        viAlias = true;
+        vimAlias = true;
+        withNodeJs = false;
+        withPython3 = false;
+        withRuby = false;
+        extraPython3Packages = false;
+        configure = {
+          customRC = ''
+            lua << EOF
+              package.path = "${self}/?.lua;" .. package.path
+            ''
+            + pkgs.lib.readFile ./init.lua
+            + ''
+              EOF
+          '';
+        };
+      };
+      packages.default = packages.envim;
+      apps.envim = flake-utils.lib.mkApp {
+        drv = packages.envim;
+        name = "envim";
+        exePath = "/bin/nvim";
+      };
+      apps.default = apps.envim;
+      devShell = mkShell {
+        buildInputs = [packages.envim];
+      };
+    });
+}
+
