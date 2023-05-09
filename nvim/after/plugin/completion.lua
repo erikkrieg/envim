57d7824b7e73e6895fe1990517b5631826dfee03
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-08 20:31:32 -0400

    adds after plugin configs back into source
    they got caught be gitignore after their directory was changed

---
+++ nvim/after/plugin/completion.lua
@@ -0,0 +1,100 @@
+print("Load after/plugin/completion")
+
+local cmp = require'cmp'
+local lspkind = require('lspkind')
+local luasnip = require('luasnip')
+
+-- Used by formatter
+local source_mapping = {
+  buffer = '[Buffer]',
+  nvim_lsp = '[LSP]',
+  nvim_lua = "[Lua]",
+  cmp_tabnine = '[TN]',
+  cmdline = '[Cmd]',
+  path = "[Path]",
+}
+
+local function formatTabnine(entry, vim_item)
+  vim_item.kind = lspkind.presets.default[vim_item.kind]
+  local menu = source_mapping[entry.source.name]
+  if entry.source.name == 'cmp_tabnine' then
+    local data = entry.completion_item.data
+    if data ~= nil and data.detail ~= nil then
+      menu = data.detail .. ' ' .. menu
+    end
+    vim_item.kind = ''
+  end
+  vim_item.menu = menu
+  return vim_item
+end
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm(),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'cmp_tabnine' },
+  }, {
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 200,
+      before = formatTabnine,
+    })
+	},
+})
+
+-- Set configuration for specific filetype.
+-- cmp.setup.filetype('gitcommit', {
+--   sources = cmp.config.sources({
+--     { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+--   }, {
+--     { name = 'buffer' },
+--   })
+-- })
+
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline', keyword_length = 2 }
+  })
+})
+
+-- Tabnine AI completions
+local tabnine = require("cmp_tabnine.config")
+tabnine:setup({
+	max_lines = 1000,
+	max_num_results = 20,
+	sort = true,
+	run_on_every_keystroke = true,
+  show_prediction_strength = true,
+	snippet_placeholder = "",
+})
---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,77 @@
+print("Load after/plugin/lsp")
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, bufopts)
+end
+
+local function config(c)
+  cfg = c or {}
+  cfg.on_attach = on_attach
+  cfg.capabilities = capabilities
+  return cfg
+end
+
+-- Rust
+require('rust-tools').setup({
+    server = config({
+      settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = {
+            command = "clippy"
+          },
+        }
+      }
+    }),
+})
+
+-- Go
+require("lspconfig").gopls.setup(config({
+	cmd = { "gopls", "serve" },
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+},
+}))
+
+-- Bash
+require("lspconfig").bashls.setup(config({}))
+
+-- Terraform
+require("lspconfig").terraformls.setup(config({}))
+require("lspconfig").tflint.setup(config({}))
+
+-- Nim
+require'lspconfig'.nimls.setup(config({
+  settings = {
+    filetypes = { "*.nim", "nim" }
+  }
+}))
+
+-- Python
+require("lspconfig").pyright.setup(config({}))
+
