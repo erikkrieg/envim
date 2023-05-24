82fd2013b47dbed4d5670343d50eb3a027be1408
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-24 00:45:55 -0400

    adds keybinds to help navigate tabs

--- nvim/lua/erikkrieg/keymaps.lua
+++ nvim/lua/erikkrieg/keymaps.lua
@@ -12,6 +12,21 @@ keymap("n", "<leader>t", "<cmd>Telescope<cr>", opts)
 keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>g", "<cmd>Telescope live_grep<cr>", opts)
 
+-- Tab navigation
+local function keymap_tab_n(n)
+  -- ":<C-u><cr>" clears any count
+  keymap("n", "<leader>" .. n, ":<C-u><cr>" .. n .. "gt", opts)
+end
+
+for n = 1, 9 do
+  keymap_tab_n(n)
+end
+
+keymap("n", "th", "<cmd>tabfirst<cr>", opts)
+keymap("n", "tk", "<cmd>tabnext<cr>", opts)
+keymap("n", "tj", "<cmd>tabprev<cr>", opts)
+keymap("n", "tl", "<cmd>tablast<cr>", opts)
+
 -- Insert
 keymap("i", "<M-BS>", "<c-w>", opts)
 
