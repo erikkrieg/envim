153850fe0a7fcd6a2c8d797423a825fe9b247064
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-14 02:09:18 -0400

    installs indent-blankline plugin

--- nvim/lua/erikkrieg/plugins.lua
+++ nvim/lua/erikkrieg/plugins.lua
@@ -57,6 +57,16 @@ require("packer").startup(function(use)
       })
     end,
   })
+  use({
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      vim.opt.list = true
+      vim.opt.listchars:append("eol:↴")
+      require("indent_blankline").setup({
+        show_end_of_line = true,
+      })
+    end,
+  })
 
   -- Specific Languages
   use("simrat39/rust-tools.nvim")
