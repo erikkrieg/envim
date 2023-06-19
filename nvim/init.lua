529417fe51bb473467e9596a7a22185f131f4f27
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-19 08:58:21 -0400

    adds a couple general purpose autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ require("config.lazy")
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
+    require("config.autocmds")
     require("config.keymaps")
   end,
 })
