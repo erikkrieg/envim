ec691d358fee029bd3b030401e87e643d9b5ae37
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-19 08:48:12 -0400

    adds keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,9 @@
 require("config.options") -- Some options need to be set before lazy
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.keymaps")
+  end,
+})
