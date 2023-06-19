ec933c37867fb12817e8a52163becffe33a84f1c
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-19 00:23:40 -0400

    configures lsp for lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,3 @@
 return {
   "nvim-lua/plenary.nvim",
-  "folke/neodev.nvim",
 }
---
+++ nvim/lua/plugins/lsp/init.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    event = "BufReadPre",
+    dependencies = {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+      { "folke/neodev.nvim", config = true },
+      { "neovim/nvim-lspconfig" },
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "L3MON4D3/LuaSnip" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-cmdline" },
+    },
+    config = function()
+      require("plugins.lsp.servers").setup()
+    end,
+  },
+}
