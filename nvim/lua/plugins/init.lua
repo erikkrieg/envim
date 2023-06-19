2005a583306bc429a7352ce3fba017d31b603927
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-18 21:04:36 -0400

    configures which-key

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,15 @@
 return {
+  "folke/neodev.nvim",
   {
     "folke/which-key.nvim",
     lazy = false,
-  },
-  {
-    "folke/neodev.nvim",
-    lazy = false,
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+      require("which-key").setup({
+        -- Additional configuration can be set here
+        -- https://github.com/folke/which-key.nvim#%EF%B8%8F-configuration
+      })
+    end,
   },
 }
