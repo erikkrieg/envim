943eef7d45c663677dbf078d5585a7c38f467505
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-18 22:49:23 -0400

    installs and configures telescope
    some additional which-key setup

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,14 +3,22 @@ return {
   "folke/neodev.nvim",
   {
     "folke/which-key.nvim",
-    lazy = false,
+    event = "VeryLazy",
     config = function()
       vim.o.timeout = true
-      vim.o.timeoutlen = 500
-      require("which-key").setup({
-        -- Additional configuration can be set here
-        -- https://github.com/folke/which-key.nvim#%EF%B8%8F-configuration
+      vim.o.timeoutlen = 750
+      local wk = require("which-key")
+      wk.setup({
+        show_help = false,
+        plugins = { spelling = true },
+        key_labels = { ["<leader>"] = "SPC" },
+        triggers = "auto",
       })
+      wk.register({
+        f = {
+          name = "+Telescope",
+        },
+      }, { prefix = "<leader>" })
     end,
   },
 }
