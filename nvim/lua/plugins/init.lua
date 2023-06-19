fc44fe618f36ea805395a22acd3ea135a84299a5
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-19 01:13:37 -0400

    installs comment plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,3 +1,10 @@
 return {
   "nvim-lua/plenary.nvim",
+  {
+    "numToStr/Comment.nvim", -- Multi-line comments
+    event = "BufReadPre",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
 }
