7c65690acaad63da5bfc8f1adc69d7980b5df1c7
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-20 09:50:36 -0400

    extracts lsp server config function into submod

---
+++ nvim/lua/plugins/lsp/servers/config.lua
@@ -0,0 +1,41 @@
+local M = {}
+
+M.on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Mappings.
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set(
+    "n",
+    "<leader>wr",
+    vim.lsp.buf.remove_workspace_folder,
+    bufopts
+  )
+  vim.keymap.set("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>F", vim.lsp.buf.format, bufopts)
+end
+
+M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+M.config = function(c)
+  local cfg = c or {}
+  cfg.on_attach = M.on_attach
+  cfg.capabilities = M.capabilities
+  return cfg
+end
+
+return M
