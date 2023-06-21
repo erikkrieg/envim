75e36fd1a8182ca7717badca3d60ab91f1a07fed
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-21 12:01:26 -0400

    migrates remaining lsp configs over

--- nvim/lua/plugins/lsp/servers/init.lua
+++ nvim/lua/plugins/lsp/servers/init.lua
@@ -4,8 +4,10 @@ M.setup = function()
   local lsp = require("lspconfig")
   local config = require("plugins.lsp.servers.config").config
 
+  -- Lua
   lsp.lua_ls.setup(config(require("plugins.lsp.servers.lua")))
 
+  -- Rust
   require("rust-tools").setup({
     server = config({
       settings = {
@@ -18,8 +20,8 @@ M.setup = function()
     }),
   })
 
+  -- Go
   require("lspconfig").gopls.setup(config({
-
     cmd = { "gopls", "serve" },
     settings = {
       gopls = {
@@ -30,6 +32,39 @@ M.setup = function()
       },
     },
   }))
+
+  -- Bash
+  require("lspconfig").bashls.setup(config({}))
+
+  -- Terraform
+  require("lspconfig").terraformls.setup(config({}))
+  require("lspconfig").tflint.setup(config({}))
+
+  -- Python
+  require("lspconfig").pyright.setup(config({}))
+
+  -- TS/JS
+  require("lspconfig").tsserver.setup(config({}))
+
+  -- Nix
+  require("lspconfig").nil_ls.setup(config({}))
+
+  -- Yaml
+  require("lspconfig").yamlls.setup(config({
+    settings = {
+      yaml = {
+        keyOrdering = false,
+        -- Issues with schemas for different k8s versions:
+        -- https://github.com/redhat-developer/yaml-language-server/issues/211
+        schemas = {
+          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        },
+      },
+    },
+  }))
+
+  -- Markdown
+  require("lspconfig").marksman.setup(config({}))
 end
 
 return M
