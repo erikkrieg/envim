9853c7798a2c78a364d7132be35c7ca8de8c49a0
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-18 20:01:21 -0500

    adds a comment for a potential change I might make to oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,6 +4,7 @@ local function config()
       show_hidden = true,
     },
   })
+  -- Consider setting `--float`
   vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 end
 
