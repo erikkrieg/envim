3edccb0b6dbad06593040ff2e816e5cdfdcb5262
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2025-01-01 13:00:12 -0500

    use vim-just from nixpkgs

--- Justfile
+++ Justfile
@@ -1,6 +1,8 @@
 alias v := dev
 alias vim := dev
 alias nvim := dev
+
 # Execute local nvim source in a nix dev shell
 dev *NVIM_ARGS:
   nix develop --impure -c nvim {{NVIM_ARGS}}
+
--- nix/plugins.nix
+++ nix/plugins.nix
@@ -1,15 +1,5 @@
 { pkgs, unpkgs, ... }: with pkgs;
 let
-  vim-just = vimUtils.buildVimPlugin {
-    name = "vim-just";
-    pname = "vim-just";
-    src = fetchFromGitHub {
-      owner = "NoahTheDuke";
-      repo = "vim-just";
-      rev = "3451e22daade268f99b1cfeb0d9fe39f4ddc06d5";
-      sha256 = "sha256-2pzdtMGdmCTprkPslGdlEezdQ6dTFrhqvz5Sc8DN3Ts";
-    };
-  };
   vim-helm = vimUtils.buildVimPlugin {
     name = "vim-helm";
     pname = "vim-helm";
