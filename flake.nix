b4baa2d3f558ea43a2ec4a3c89ea4c9b13256583
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2025-10-19 14:16:06 -0400

    chore: upgrade nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Erik's Neovim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
