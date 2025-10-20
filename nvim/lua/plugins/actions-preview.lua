6ac1de4fb12f7111a18cdeafc8f747def3c75ca5
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2025-10-20 01:28:47 -0400

    fix: fmt issues

--- nvim/lua/plugins/actions-preview.lua
+++ nvim/lua/plugins/actions-preview.lua
@@ -1,7 +1,12 @@
 local function config()
   local ap = require("actions-preview")
   ap.setup()
-  vim.keymap.set({ "v", "n" }, "<leader>fa", ap.code_actions, { desc = "Code actions preview" })
+  vim.keymap.set(
+    { "v", "n" },
+    "<leader>fa",
+    ap.code_actions,
+    { desc = "Code actions preview" }
+  )
 end
 
 return {
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -7,12 +7,36 @@ return {
     local wk = require("which-key")
     wk.add({
       { "<leader>x", group = "Diagnostics" },
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle focus=true<cr>", desc = "Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>", desc = "Buffer Diagnostics (Trouble)" },
-      { "<leader>xs", "<cmd>Trouble symbols toggle focus=true<cr>", desc = "Symbols (Trouble)" },
-      { "<leader>xl", "<cmd>Trouble lsp toggle focus=true win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
-      { "<leader>xL", "<cmd>Trouble loclist toggle focus=true<cr>", desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>Trouble qflist toggle focus=true<cr>", desc = "Quickfix List (Trouble)" },
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle focus=true<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xs",
+        "<cmd>Trouble symbols toggle focus=true<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>xl",
+        "<cmd>Trouble lsp toggle focus=true win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle focus=true<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle focus=true<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     })
   end,
 }
