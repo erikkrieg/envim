fcf2019480c39f9eb3da4f3a31083f86744f2930
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2025-10-20 00:09:14 -0400

    refactor: use wk add instead of keymap set

--- nvim/lua/plugins/lsp/servers/config.lua
+++ nvim/lua/plugins/lsp/servers/config.lua
@@ -3,30 +3,35 @@ local M = {}
 M.on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  local map_n = function(lhs, rhs, desc)
-    local opts = { noremap = true, silent = true, buffer = bufnr, desc = desc }
-    vim.keymap.set("n", lhs, rhs, opts)
-  end
-
-  map_n("gD", vim.lsp.buf.declaration, "Go to declaration")
-  map_n("gd", vim.lsp.buf.definition, "Go to definition")
-  map_n("K", vim.lsp.buf.hover, "Hover documentation")
-  map_n("gi", vim.lsp.buf.implementation, "Go to implementation")
-  map_n("<C-k>", vim.lsp.buf.signature_help, "Signature help")
-  map_n("<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
-  map_n(
-    "<leader>wr",
-    vim.lsp.buf.remove_workspace_folder,
-    "Remove workspace folder"
-  )
-  map_n("<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "List workspace folders")
-  map_n("<leader>D", vim.lsp.buf.type_definition, "Type definition")
-  map_n("<leader>rn", ":IncRename ", "Rename symbol")
-  map_n("<leader>ca", vim.lsp.buf.code_action, "Code actions")
-  map_n("gr", vim.lsp.buf.references, "References")
-  map_n("<leader>F", vim.lsp.buf.format, "Format buffer")
+  local wk = require("which-key")
+  wk.add({
+    buffer = bufnr,
+    { "gD", vim.lsp.buf.declaration, desc = "Go to declaration" },
+    { "gd", vim.lsp.buf.definition, desc = "Go to definition" },
+    { "K", vim.lsp.buf.hover, desc = "Hover documentation" },
+    { "gi", vim.lsp.buf.implementation, desc = "Go to implementation" },
+    { "gr", vim.lsp.buf.references, desc = "References" },
+    { "<C-k>", vim.lsp.buf.signature_help, desc = "Signature help" },
+    { "<leader>w", group = "Workspace" },
+    { "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add folder" },
+    {
+      "<leader>wr",
+      vim.lsp.buf.remove_workspace_folder,
+      desc = "Remove folder",
+    },
+    {
+      "<leader>wl",
+      function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end,
+      desc = "List folders",
+    },
+    { "<leader>c", group = "Code" },
+    { "<leader>ca", vim.lsp.buf.code_action, desc = "Actions" },
+    { "<leader>D", vim.lsp.buf.type_definition, desc = "Type definition" },
+    { "<leader>rn", ":IncRename ", desc = "Rename symbol" },
+    { "<leader>F", vim.lsp.buf.format, desc = "Format buffer" },
+  })
 end
 
 M.capabilities = require("cmp_nvim_lsp").default_capabilities()
